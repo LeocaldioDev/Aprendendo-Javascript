3de0ffd58f2b075bf9a43249882420c76811a875
Leocaldio Carlos <leocaldiocarloss@gmail.com>
2025-03-11 06:23:07 +0800

    aprendendo array 1

--- Aprendendo Javascript/Js_Script.js
+++ Aprendendo Javascript/Js_Script.js
@@ -1,87 +1,108 @@
-const caixaCursos=document.querySelector("#caixaCursos")
-const btn_c=[...document.querySelectorAll(".curso")]
-const c1_2=document.querySelector("#c1_2")
-const cursos=["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
-const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
-const btnRemoverCurso=document.getElementById("btnRemoverCurso")
-const btnAdicionarNovoCursoAntes=document.getElementById("btnAdicionarNovoCursoAntes")
-const btnAdicionarNovoCursoDepois=document.getElementById("btnAdicionarNovoCursoDepois")
-const nomeCurso=document.getElementById("nomeCurso")
+const caixa=document.querySelector("#caixa")
 
-let indice=0
+let cores=["azul","verde","vermelho",["claro","escuro","médio"]]
+let cursos=["HTML","CSS","Javascript",cores]
 
-const tirarSelecao=()=>{
-    const cursosSelecionados=[...document.querySelectorAll(".selecionado")]
-    cursosSelecionados.map((el)=>{
-        el.classList.remove("selecionado")
-    })
-}
+//cursos[0]=2023
 
-const criarNovoCurso=(curso)=>{
-    const novoElemento=document.createElement("div")
-    novoElemento.setAttribute("id","c"+indice)
-    novoElemento.setAttribute("class","curso c1")
-    novoElemento.innerHTML=curso
-    novoElemento.addEventListener("click",(evt)=>{
-        tirarSelecao()
-        evt.target.classList.toggle("selecionado")
-    })
-    return novoElemento
-}
+// cursos.push("C++")
+// cursos.unshift("Python")
+// cursos.shift()
 
-cursos.map((el,chave)=>{
-    const novoElemento=criarNovoCurso(el)
-    caixaCursos.appendChild(novoElemento)
-    indice++ 
+console.log(cursos[3][3][2])
+
+cursos.map((el)=>{
+    let p=document.createElement("p")
+    p.innerHTML=el
+    caixa.appendChild(p)
 })
+//----------------------------------------------------------------------------------------------------
 
-const cursoSelecionado=()=>{
-    const cursosSelecionados=[...document.querySelectorAll(".selecionado")]
-    return cursosSelecionados[0]
-}
+// const caixaCursos=document.querySelector("#caixaCursos")
+// const btn_c=[...document.querySelectorAll(".curso")]
+// const c1_2=document.querySelector("#c1_2")
+// const cursos=["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
+// const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
+// const btnRemoverCurso=document.getElementById("btnRemoverCurso")
+// const btnAdicionarNovoCursoAntes=document.getElementById("btnAdicionarNovoCursoAntes")
+// const btnAdicionarNovoCursoDepois=document.getElementById("btnAdicionarNovoCursoDepois")
+// const nomeCurso=document.getElementById("nomeCurso")
 
-btnCursoSelecionado.addEventListener("click",(evt)=>{
-    try{
-        alert("Curso selecionado: " + cursoSelecionado().innerHTML)
-    }catch(ex){
-        alert("Selecione um curso")
-    }
-})
+// let indice=0
 
-btnRemoverCurso.addEventListener("click",(evt)=>{
-    const cs=cursoSelecionado()
-    if(cs!=undefined){
-        cs.remove()
-    }else{
-        alert("Selecione um curso")
-    }
-})
+// const tirarSelecao=()=>{
+//     const cursosSelecionados=[...document.querySelectorAll(".selecionado")]
+//     cursosSelecionados.map((el)=>{
+//         el.classList.remove("selecionado")
+//     })
+// }
 
-btnAdicionarNovoCursoAntes.addEventListener("click",(evt)=>{
-    try{
-        if(nomeCurso.value!=""){
-            const novoCurso=criarNovoCurso(nomeCurso.value)
-            caixaCursos.insertBefore(novoCurso,cursoSelecionado())
-        }else{
-            alert("Digite o nome do curso")
-        }
-    }catch(ex){
-        alert("Selecione um curso")
-    }
-})
 
-btnAdicionarNovoCursoDepois.addEventListener("click",(evt)=>{
-    try{
-        if(nomeCurso.value!=""){
-            const novoCurso=criarNovoCurso(nomeCurso.value)
-            caixaCursos.insertBefore(novoCurso,cursoSelecionado().nextSibling)
-        }else{
-            alert("Digite o nome do curso")
-        }
-    }catch(ex){
-        alert("Selecione um curso")
-    }
-})
+// const criarNovoCurso=(curso)=>{
+//     const novoElemento=document.createElement("div")
+//     novoElemento.setAttribute("id","c"+indice)
+//     novoElemento.setAttribute("class","curso c1")
+//     novoElemento.innerHTML=curso
+//     novoElemento.addEventListener("click",(evt)=>{
+//         tirarSelecao()
+//         evt.target.classList.toggle("selecionado")
+//     })
+//     return novoElemento
+// }
+
+// cursos.map((el,chave)=>{
+//     const novoElemento=criarNovoCurso(el)
+//     caixaCursos.appendChild(novoElemento)
+//     indice++ 
+// })
+
+// const cursoSelecionado=()=>{
+//     const cursosSelecionados=[...document.querySelectorAll(".selecionado")]
+//     return cursosSelecionados[0]
+// }
+
+// btnCursoSelecionado.addEventListener("click",(evt)=>{
+//     try{
+//         alert("Curso selecionado: " + cursoSelecionado().innerHTML)
+//     }catch(ex){
+//         alert("Selecione um curso")
+//     }
+// })
+
+// btnRemoverCurso.addEventListener("click",(evt)=>{
+//     const cs=cursoSelecionado()
+//     if(cs!=undefined){
+//         cs.remove()
+//     }else{
+//         alert("Selecione um curso")
+//     }
+// })
+
+// btnAdicionarNovoCursoAntes.addEventListener("click",(evt)=>{
+//     try{
+//         if(nomeCurso.value!=""){
+//             const novoCurso=criarNovoCurso(nomeCurso.value)
+//             caixaCursos.insertBefore(novoCurso,cursoSelecionado())
+//         }else{
+//             alert("Digite o nome do curso")
+//         }
+//     }catch(ex){
+//         alert("Selecione um curso")
+//     }
+// })
+
+// btnAdicionarNovoCursoDepois.addEventListener("click",(evt)=>{
+//     try{
+//         if(nomeCurso.value!=""){
+//             const novoCurso=criarNovoCurso(nomeCurso.value)
+//             caixaCursos.insertBefore(novoCurso,cursoSelecionado().nextSibling)
+//         }else{
+//             alert("Digite o nome do curso")
+//         }
+//     }catch(ex){
+//         alert("Selecione um curso")
+//     }
+// })
 // parentNode
 // childNodes[nodenumber]
 // firstChild
